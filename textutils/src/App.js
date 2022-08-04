4a8ab8b55522f3c0cb95274dc23a1269705555c2
Prabhas Mondal <48164476+pmondal98@users.noreply.github.com>
2022-08-05 00:10:08 +0530

    7th

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
+    <>
     <Navbar title="TextUtils" about="About Us"/>
+    <div className="container">
+    <TextForm/>
+    </div>
+    </>
   );
 }
 
---
+++ textutils/src/components/TextForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TextForm() {
+  return (
+    <div class="mb-3">
+        <label for="myBox" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="myBox" rows="3"></textarea>
+    </div>
+  )
+}
