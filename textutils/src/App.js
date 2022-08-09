6aae1c16d88d085d8fc44f42b4baf27e2098840f
Prabhas Mondal <48164476+pmondal98@users.noreply.github.com>
2022-08-09 20:22:57 +0530

    10th

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
-    <Navbar title="TextUtils" about="About Us"/>
-    <div className="container">
-    <TextForm heading="Enter your text to analyze"/>
-    </div>
+      <Navbar title="TextUtils" about="About Us" />
+      <div className="container">
+        <TextForm heading="Enter your text to analyze" />
+        {/* <About /> */}
+      </div>
     </>
   );
 }
